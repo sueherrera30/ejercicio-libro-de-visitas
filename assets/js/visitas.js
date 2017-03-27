b134427f472e4b63cfff37e419c23876f365e6ef
sue martinez <sueherrera30@gmail.com>
2017-03-27 15:08:12 +0000

    colores

--- assets/js/visitas.js
+++ assets/js/visitas.js
@@ -17,43 +17,47 @@ function vizualizacion(){
       }
 
 function colorTexto(){
-  var color =prompt("¿Qué color deseas implementar?\n a)ciruela(#DDA0DD) b)verde hoja(#3CB371) c)negrito(#000000)\n << ingresa el no. que esta entre parentesis>> ");
-  if (color == "#DDA0DD") {
-   var ciruela = document.getElementById("modificado").style.color = "#DDA0DD";
-   ciruela.innerHTML;
- }
- else if (color == "#3CB371") {
-   var verde= document.getElementById("modificado").style.color = "#3CB371";
-   verde.innerHTML;
-   }
-  else if (color == "#000000") {
-    var negro =document.getElementById("modificado").style.color = "#000000";
-    salmon.innerHTML;
-  }
-  else{
-    alert("esta opción no esta disponible");
-  }
-
-
- }
+  var texto = document.getElementById('modificado');
+  var color = prompt("ingresa un numero de color Hexadecimal");
+  // var color =prompt("¿Qué color deseas implementar?\n a)ciruela(#DDA0DD) b)verde hoja(#3CB371) c)negrito(#000000)\n << ingresa el no. que esta entre parentesis>> ");
+  texto.style.color = color;
+}
+ //  if (color == "#DDA0DD") {
+ //   var ciruela = document.getElementById("modificado").style.color = "#DDA0DD";
+ //   ciruela.innerHTML;
+ // }
+ // else if (color == "#3CB371") {
+ //   var verde= document.getElementById("modificado").style.color = "#3CB371";
+ //   verde.innerHTML;
+ //   }
+ //  else if (color == "#000000") {
+ //    var negro =document.getElementById("modificado").style.color = "#000000";
+ //    salmon.innerHTML;
+ //  }
+ //  else{
+ //    alert("esta opción no esta disponible");
+ //  }
 
 function colorFondo(){
-  var color =prompt("¿Qué color deseas implementar?\n a)marron(#BC8F8F) b)Lavanda(#D8BFD8) c)mandarina dulce (#FFA07A)\n << ingresa el no. que esta entre parentesis>> ");
-
-if (color == "#BC8F8F") {
-  var marron = document.getElementById("modificado").style.background= "#BC8F8F";
-   }
-else if (color == "#D8BFD8") {
-   var lavanda = document.getElementById("modificado").style.background=  "#D8BFD8";
-   }
-else if (color == "#FFA07A") {
-   var mandarina = document.getElementById("modificado").style.background= "#FFA07A";
-   }
-  else{
-    alert("esta opción no esta disponible");
-  }
-
+  var texto = document.getElementById('modificado');
+    var color = prompt("ingresa un numero de color Hexadecimal");
+  // var color =prompt("¿Qué color deseas implementar?\n a)marron(#BC8F8F) b)Lavanda(#D8BFD8) c)mandarina dulce (#FFA07A)\n << ingresa el no. que esta entre parentesis>> ");
+  texto.style.background =color;
 }
+// if (color == "#BC8F8F") {
+//   var marron = document.getElementById("modificado").style.background= "#BC8F8F";
+//    }
+// else if (color == "#D8BFD8") {
+//    var lavanda = document.getElementById("modificado").style.background=  "#D8BFD8";
+//    }
+// else if (color == "#FFA07A") {
+//    var mandarina = document.getElementById("modificado").style.background= "#FFA07A";
+//    }
+//   else{
+//     alert("esta opción no esta disponible");
+//   }
+//
+ //}
 
 function centrar(){
  document.getElementById("modificado").style.textAlign = "center";
@@ -68,10 +72,12 @@ function alinearIzquierda(){
 
 
 function agregarComentario(){
- var nuevoElemento = document.createElement('p');
-  var lista = document.getElementById('comentarios');
-  var textoEditado = document.getElementById('modificado').innerHTML;
-  nuevoElemento.innerHTML = textoEditado;
-  lista.appendChild(nuevoElemento);
-  
+ // var nuevoElemento = document.createElement('p');
+ //  var lista = document.getElementById('comentarios');
+ //  var textoEditado = document.getElementById('modificado').innerHTML;
+ //  nuevoElemento.innerHTML = textoEditado;
+ //  lista.appendChild(nuevoElemento);
+var element = document.getElementById('modificado').innerHTML;
+var nuevo = element.cloneNode();
+  element.appendChild(nuevo);
 }
